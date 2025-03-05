6130ec178e44190b4aeed5fb1d4787582bb4380f
JaeHo Song <thruthesky@gmail.com>
2025-03-05 16:28:14 +0800

    adding playwright test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -4,5 +4,5 @@ test("has title", async ({ page }) => {
   await page.goto("/");
 
   // Expect a title "to contain" a substring.
-  await expect(page.locator("h5")).toBeVisible();
+  await expect(page.locator("a")).toBeVisible();
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -2,20 +2,13 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./e2e",
-
   fullyParallel: true,
-
   forbidOnly: !!process.env.CI,
-
   retries: process.env.CI ? 2 : 0,
-
   workers: process.env.CI ? 1 : undefined,
-
   reporter: "html",
-
   use: {
     baseURL: "http://127.0.0.1:3000/ci4/",
-
     trace: "on-first-retry",
   },
 
@@ -29,6 +22,5 @@ export default defineConfig({
   webServer: {
     command: "npm run start",
     url: "http://127.0.0.1:3000/ci4/",
-    //   reuseExistingServer: !process.env.CI,
   },
 });
